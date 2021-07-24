a5ab91abcefa7e2af869bd9d4a3ea280c6c24465
Shantnu12345 <shantnukakkar1992@gmail.com>
2021-07-24 01:56:43 -0400

    Removed unecessary things

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,6 @@
 
 using namespace std;
 
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("]");
-  if (found_null != std::string::npos) {
-    return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
-}
-
 int main()
 {
   uWS::Hub h;
